a488c726f9b8507912999330435ca78b16ce5a4b
LittlefieldBous <amybousamra.com>
2014-10-02 20:44:15 -0400

    updates mad lib

--- madlib_lab1/main.py
+++ madlib_lab1/main.py
@@ -82,14 +82,14 @@ food = dict() #create dictionary object
 food = {"green":"gummy worms", "blue":"eyeballs","red":"bat wings"}
 
 #PRINT STORY--------------
-print "It was a cold," + str(adj1) + " " + "Halloween night." + str(name1) + "and" + str(name2) + " " + \
+print "It was a cold," + str(adj1) + " " + "Halloween night." + str(name1) + " " + "and" + str(name2) + " " + \
       "were driving around looking for a classmates Halloween party." + " " + "Hoping they would win first prize for best " \
       "costume," + " " + str(name1) + " " + "was dressed up as a" + str(costume1) + "." + "While" + " " + str(name2) + " " + \
       "thought he would compliment her outfit by dressing as a" + " " + str(costume2) + "." + " " + "Suddenly the car made a loud" + " " + \
-      str(sound1)+ " " + str(name1) + " " + "and" + " " + str(name2) + "realized they were out of gas!" + " " + str(name1) + " " + "and" + str(name2) +  " " + \
+      str(sound1)+ "!!" + str(name1) + " " + "and" + " " + str(name2) + "realized they were out of gas!" + " " + str(name1) + " " + "and" + str(name2) +  " " + \
       "grabbed their cell phones but there was no signal." + "They realized they were stuck in the middle of a" + " " + str(environment) + "." + str(name1) + \
       " " + "spotted a" + " " + str(house) + "in the distance with the lights on." + "The two friends decided to knock on the door for help." + \
-      str(name1) + "knocked on the door." + "A person wearing a mask was dressed as a" + adjectives[adj2] + str(job) + " " + "opened the door." + \
+      str(name1) + "knocked on the door." + "A person wearing a mask was dressed as a" + " " + adjectives[adj2] + " " + str(job) + " " + "opened the door." + \
       "hello...may I help you?" + str(name2) + " " + "replied yes, our car broke down and we wondered if we could use your phone to call for help?" + " " +\
       "Certainly replied the" + str(job) + "." + "Let me get you my phone and some" +  str(color) +  " " + food["red"] + "and" + str(drink) + "while you wait." + "The" + " " + \
       adjectives[adj2] + " " + str(job) + "disappeared into the kitchen." + " As" + str(name1) + " " + "and" + " " + str(name2) + " " +\
@@ -110,7 +110,7 @@ def calcArea(n,y):
 a = calcArea(n, 2);
 
 
-print "When they looked out the window they thought they saw a" + str(monster) + "!" + " " + "The creature appeared to be" + str(a) + "feet tall!" +\
+print "When they looked out the window they thought they saw a" + str(monster) + "!" + " " + "The creature appeared to be" + " " + str(a) + "feet tall!" +\
     "-Frightened they ran for the door but they couldn't get out." + "They both screamed out in unison..."
 
 #For LOOP----
